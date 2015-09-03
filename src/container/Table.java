426ff565562fc91f5700b46d836b0713823cded0
Xiaoxi Guo <xguo0122@gmail.com>
2015-09-03 15:38:05 -0500

    interface of table

--- src/container/Table.java
+++ src/container/Table.java
@@ -22,4 +22,34 @@ public interface Table<K, V> {
      * @return The value associated if it is exist, or else null.
      */
     abstract public V get(K key);
+
+
+    /**
+     * Check if the table contains the specific key.
+     * @param key The specific key to check.
+     * @return true if the key is in the table.
+     */
+    abstract public boolean contains(K key);
+
+
+    /**
+     * Remove the key-value pair for a specific key from the table.
+     * @param key The specific key to be removed.
+     * @return The value to which the specific value is mapped, null if key not found.
+     */
+    abstract public V remove(K key);
+
+
+    /**
+     * Check if the table is empty.
+     * @return true if it is empty.
+     */
+    abstract public boolean isEmpty();
+
+
+    /**
+     * Return the number of key-value pairs in the table.
+     * @return The number of key-value pairs.
+     */
+    abstract public int size();
 }
