44af1a2fc44a43a65d5a2f2a404a88beebfce5aa
Xiaoxi Guo <xguo0122@gmail.com>
2015-09-18 11:48:18 -0500

    modify insert BST and add iterator

--- src/container/BinarySearchTree.java
+++ src/container/BinarySearchTree.java
@@ -1,9 +1,11 @@
 package container;
 
+import java.util.Iterator;
+
 /**
  * General Binary Search Tree implemetation.
  */
-public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
+public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> implements Iterable<E> {
 
     /**
      * Search a specific element in the BST.
@@ -27,16 +29,26 @@ public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
      * @param item The element to insert.
      */
     public void insert(E item) {
-        insert(this.root, item);
+        root = insert(root, item);
     }
-    private void insert(Node<E> root, E item) {
+    private Node<E> insert(Node<E> root, E item) {
         if (root == null) {
-            root = new Node<E>(item);
+            return new Node<>(item);
         }
         if (item.compareTo(root.data) < 0) {
-            insert(root.left, item);
+            root.left = insert(root.left, item);
         } else {
-            insert(root.right, item);
+            root.right = insert(root.right, item);
         }
+        return root;
+    }
+
+
+    /**
+     * Return an iterator of all elements in ascendant order.
+     * @return An ascendant order.
+     */
+    public Iterator<E> iterator() {
+        return inorderIterator();
     }
 }
--- src/container/TestContainer.java
+++ src/container/TestContainer.java
@@ -134,6 +134,21 @@ class TestBinaryTree {
     }
 }
 
+class TestBinarySearchTree {
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.insert(30);
+        bst.insert(20);
+        bst.insert(40);
+        bst.insert(70);
+        bst.insert(60);
+        bst.insert(80);
+        for (Integer i : bst) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
 
 class TestResizableArray {
     public static void main(String[] args) {
